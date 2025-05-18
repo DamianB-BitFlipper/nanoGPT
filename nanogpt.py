576abe9acbf44ba44a957ff89ec7c9fe73cbc61f
Damian Barabonkov <damianb@alum.mit.edu>
2025-05-18 20:00:30 +0200

    GPT-2 runs!

--- nanogpt.py
+++ nanogpt.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import math
 from typing import ClassVar, Self
 
+import tiktoken
 import torch
 import torch.nn as nn
 import torch.nn.functional as F  # noqa: N812
@@ -132,6 +133,30 @@ class GPT(nn.Module):
         )
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+    def forward(self, idx: torch.Tensor) -> torch.Tensor:
+        # The `idx` is of shape (B, T)
+        B, T = idx.size()  # noqa: N806
+        assert T <= self.config.block_size, (
+            f"Cannot forward sequences of length {T}, block size is {self.config.block_size}"
+        )
+
+        # Forward the token and position embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)  # shake (T)
+        # Position embeddings of shape (T, n_embed)
+        pos_emb = self.transformer.wpe(pos)  # type: ignore
+        # Token embeddings of shape (B, T, n_embed)
+        tok_emb = self.transformer.wte(idx)  # type: ignore
+        x = tok_emb + pos_emb
+
+        # Forward the blocks of the transformer
+        for block in self.transformer.h:  # type: ignore
+            x = block(x)
+
+        # Forward the final `LayerNorm` and the classifier
+        x = self.transformer.ln_f(x)  # type: ignore
+        logits = self.lm_head(x)  # (B, T, vocab_size)
+        return logits
+
     @classmethod
     def from_pretrained(cls, model_type: str) -> GPT:
         """Loads pretrained GPT-2 model weights from huggingface"""
@@ -198,8 +223,51 @@ class GPT(nn.Module):
 
 
 def main() -> None:
-    GPT.from_pretrained("gpt2")
-    logger.info("Did not crash! Yay!")
+    num_return_sequences = 5
+    max_length = 30
+    top_k = 50
+
+    enc = tiktoken.get_encoding("gpt2")
+    model = GPT.from_pretrained("gpt2")
+    logger.info("Model loaded")
+
+    # Configures the model for inference
+    model.eval()
+
+    # Repeat the tokenized input string `num_return_sequences` times
+    x = (
+        torch.tensor(enc.encode("Hello, I'm a language model,"), dtype=torch.long)
+        .unsqueeze(0)
+        .repeat(num_return_sequences, 1)
+    )
+
+    # Generate! Right now x is (B, T) where B = 5, T = 8
+    # Set the seed to 42
+    torch.manual_seed(42)
+    torch.mps.manual_seed(42)
+    while x.size(1) < max_length:
+        # Forward the model to get the logits
+        with torch.no_grad():
+            logits = model(x)  # (B, T, vocab_size)
+            # Take the logits at the final position
+            logits = logits[:, -1, :]  # (B, vocab_size)
+            # Get the probabilities
+            probs = F.softmax(logits, dim=-1)
+            # Do top-k sampling of `top_k` (huggingface pipeline default)
+            # topk_probs here becomes (5, top_k), topk_indicies is (5, top_k)
+            topk_probs, topk_indicies = torch.topk(probs, top_k, dim=-1)
+            # Select a token from the top-k probabilities
+            ix = torch.multinomial(topk_probs, 1)  # (B, 1)
+            # Gather the corresponding indices
+            xcol = torch.gather(topk_indicies, -1, ix)  # (B, 1)
+            # Append to the sequence
+            x = torch.cat((x, xcol), dim=1)
+
+    # Print the generated text
+    for i in range(num_return_sequences):
+        tokens = x[i, :max_length].tolist()
+        decoded = enc.decode(tokens)
+        logger.info(f"> {decoded}")
 
 
 if __name__ == "__main__":
